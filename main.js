aa70c5f091c9fa29258775ec3aaadb4e9658a223
tetsutetsu0110 <153423615+tetsutetsu0110@users.noreply.github.com>
2025-08-26 22:09:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,72 +1,44 @@
-// ==UserScript==
-// @name         Garden Persist After Ascend
-// @namespace    CookieClickerMods
-// @version      1.0
-// @description  Keeps Garden state across ascensions in Cookie Clicker
-// @author       You
-// @match        https://orteil.dashnet.org/cookieclicker/*
-// @grant        none
-// ==/UserScript==
-
-(function() {
-    if (!CCSE) return;
-
-    CCSE.NewGame = function() {
-        if (GardenPersist.savedGarden) {
-            GardenPersist.loadGarden();
-        }
-    };
-
-    var GardenPersist = {
-        savedGarden: null,
-
-        init: function() {
-            Game.customAscend.push(function() {
-                GardenPersist.saveGarden();
-            });
-            Game.customReset.push(function() {
-                GardenPersist.loadGarden();
-            });
-        },
-
-        saveGarden: function() {
-            if (Game.Objects['Farm'] && Game.Objects['Farm'].minigame) {
-                var M = Game.Objects['Farm'].minigame;
-                GardenPersist.savedGarden = {
-                    plots: JSON.stringify(M.plot),
-                    soil: M.soil,
-                    nextSoil: M.nextSoil,
-                    step: M.step,
-                    unlocks: JSON.stringify(M.plants),
-                };
-            }
-        },
-
-        loadGarden: function() {
-            if (GardenPersist.savedGarden && Game.Objects['Farm'] && Game.Objects['Farm'].minigame) {
-                var M = Game.Objects['Farm'].minigame;
-                var data = GardenPersist.savedGarden;
-
-                try {
-                    M.plot = JSON.parse(data.plots);
-                    M.soil = data.soil;
-                    M.nextSoil = data.nextSoil;
-                    M.step = data.step;
-
-                    var unlocks = JSON.parse(data.unlocks);
-                    for (var id in unlocks) {
-                        if (M.plants[id]) {
-                            M.plants[id].unlocked = unlocks[id].unlocked;
-                        }
-                    }
-
-                    if (M.plotCanvas) M.plotCanvas.needRedraw = 1;
-                } catch(e) {
-                    console.error('GardenPersist load error:', e);
+(function waitCCSE(){
+    if(typeof CCSE !== 'undefined'){
+        (function(){
+            // GardenPersist MOD 本体
+            var GardenPersist = {
+                savedGarden:null,
+                save:function(){
+                    var M = Game.Objects['Farm'] && Game.Objects['Farm'].minigame;
+                    if(!M) return;
+                    this.savedGarden={
+                        plots:JSON.stringify(M.plot),
+                        soil:M.soil,
+                        nextSoil:M.nextSoil,
+                        step:M.step,
+                        unlocks:JSON.stringify(M.plants)
+                    };
+                    Game.Notify('GardenPersist','Garden saved',[16,5]);
+                },
+                load:function(){
+                    var M = Game.Objects['Farm'] && Game.Objects['Farm'].minigame;
+                    if(!M || !this.savedGarden) return;
+                    try{
+                        var data=this.savedGarden;
+                        M.plot=JSON.parse(data.plots);
+                        M.soil=data.soil;
+                        M.nextSoil=data.nextSoil;
+                        M.step=data.step;
+                        var unlocks=JSON.parse(data.unlocks);
+                        for(var id in unlocks) if(M.plants[id]) M.plants[id].unlocked=unlocks[id].unlocked;
+                        if(M.plotCanvas) M.plotCanvas.needRedraw=1;
+                        Game.Notify('GardenPersist','Garden restored',[16,5]);
+                    }catch(e){console.error(e);}
+                },
+                init:function(){
+                    Game.customAscend.push(this.save.bind(this));
+                    Game.customReset.push(this.load.bind(this));
                 }
-            }
-        }
-    };
-
-    GardenPersist.init();
+            };
+            GardenPersist.init();
+            window.GardenPersist=GardenPersist;
+            Game.Notify('GardenPersist','MOD loaded',[16,5]);
+        })();
+    } else setTimeout(waitCCSE,500);
 })();
